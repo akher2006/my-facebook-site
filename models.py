4b098cc141fc0fb682ca36f0d9c1a7bd721c3668
akher2006 <abdulakher2006@gmail.com>
2025-12-25 11:34:58 +0600

    models added

---
+++ models.py
@@ -0,0 +1,5 @@
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text, nullable=False)
+    date_posted = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
